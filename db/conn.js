18a5ebb6c24165b892642a7205fb006bf99deef5
daniveronesi <danieleveronesi9@gmail.com>
2026-04-14 11:18:26 -0300

    API Node com Mongo e autenticação

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/petshop");
+        await mongoose.connect("mongodb+srv://daniveronesi:456123@cluster0.hnz7mxz.mongodb.net/petshop");
         console.log("Conectado ao MongoDB");
     } catch (error) {
         console.error(error);
--- models/user.js
+++ models/user.js
@@ -1,9 +1,32 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-});
+const { Schema } = mongoose
 
-export default mongoose.model("User", userSchema);
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema)
+
+export default User
